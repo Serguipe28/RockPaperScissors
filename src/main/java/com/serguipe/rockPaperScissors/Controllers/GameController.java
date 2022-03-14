87e659dfbc0d7820905052d90c6e60fd2e8138cf
Sergio Guillen <serguipe28@gmail.com>
2022-03-14 11:43:25 +0100

    GameController finish

--- src/main/java/com/serguipe/rockPaperScissors/Controllers/GameController.java
+++ src/main/java/com/serguipe/rockPaperScissors/Controllers/GameController.java
@@ -5,6 +5,8 @@ import com.serguipe.rockPaperScissors.Services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,7 +18,14 @@ public class GameController {
     @GetMapping("/")
     public ModelAndView welcome(){
         ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("pc",service.play());
+
+        return modelAndView;
+    }
+    @PostMapping("/oneTwoThree")
+    public ModelAndView move(@RequestParam("trie") String intento){
+        String result = service.play(intento);
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("result",result);
         return modelAndView;
     }
 
