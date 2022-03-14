159543510f5c76e401c3c8cfb08fc747c503dc7a
Sergio Guillen <serguipe28@gmail.com>
2022-03-14 11:43:52 +0100

    GameService finish

--- src/main/java/com/serguipe/rockPaperScissors/Services/GameService.java
+++ src/main/java/com/serguipe/rockPaperScissors/Services/GameService.java
@@ -7,21 +7,51 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class GameService {
 
-    public String play(){
+    public String play(String trie){
+        String selected = pcTrie();
+        String result = "" ;
+        result = verification(trie, selected, result);
+        return result ;
 
-        List playerPc = new ArrayList();
+    }
+
+    private String verification(String trie, String selected, String result) {
+        if(Objects.equals(trie, selected)){
+            result = trie + " vs "+ selected + " = Empate";
+        }else if (Objects.equals(trie, "Rock")){
+            if (Objects.equals(selected, "Paper")){
+                result = trie +" vs " + selected + " = Gana " + selected;
+            }else {
+                result = trie +" vs " + selected + " = Gana " + trie;
+            }
+        }else if (Objects.equals(trie, "Paper")){
+            if (Objects.equals(selected, "Rock")){
+                result = trie +" vs " + selected + " = Gana " + trie;
+            }else {
+                result = trie + " vs " +selected + " = Gana " + selected;
+            }
+        }else if (Objects.equals(trie, "Scissors")){
+            if (Objects.equals(selected, "Rock")){
+                result = trie + " vs " +selected + " = Gana " + selected;
+            }else {
+                result = trie + " vs " + selected + " = Gana " + trie;
+            }
+        }
+        return result;
+    }
+
+    private String pcTrie() {
         int select = (int)(Math.random()*3);
-        playerPc.add(0, new Rock());
-        playerPc.add(1, new Paper());
-        playerPc.add(2, new Scissors());
+        List<String> playerPc = new ArrayList<String>();
+        playerPc.add(0, new Rock().getName());
+        playerPc.add(1, new Paper().getName());
+        playerPc.add(2, new Scissors().getName());
 
-
-
-        return playerPc.get(select).toString();
-
+        return playerPc.get(select);
     }
 }
